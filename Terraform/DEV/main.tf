baec08e51d76336172392f80766692a64403a83c
Alessandro Penco <108061402+apencoreply@users.noreply.github.com>
2024-01-23 18:12:59 +0100

    Update main.tf
    commented container registry

--- Terraform/DEV/main.tf
+++ Terraform/DEV/main.tf
@@ -12,13 +12,13 @@ resource "azurerm_role_assignment" "role_acrpull" {
   skip_service_principal_aad_check = true
 }
 
-resource "azurerm_container_registry" "acr" {
-  name                = var.acr_name
-  resource_group_name = azurerm_resource_group.aks-rg.name
-  location            = var.location
-  sku                 = "Standard"
-  admin_enabled       = false
-}
+# resource "azurerm_container_registry" "acr" {
+#   name                = var.acr_name
+#   resource_group_name = azurerm_resource_group.aks-rg.name
+#   location            = var.location
+#   sku                 = "Standard"
+#   admin_enabled       = false
+# }
 
 resource "azurerm_kubernetes_cluster" "aks" {
   name                = var.cluster_name
